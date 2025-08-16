8ba70fb0afe7d42ae1ec0c3fc6caa332b0c1d80b
Philipp Hossner <philipp.hossner@posteo.de>
2025-08-17 01:10:18 +0200

    fix: restore dict.py compatibility shim for OpenAPI Generator bug
    The OpenAPI Generator v7.15.0 incorrectly imports Dict from models.dict
    instead of using typing.Dict in maps_api.py and storage_api.py. This shim
    ensures all generated APIs work correctly even though our current tests
    don't exercise MapsApi or StorageApi.

---
+++ codegen/haproxy_dataplane_v3/haproxy_dataplane_v3/models/dict.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+"""
+Compatibility shim for OpenAPI Generator v7.15.0 bug.
+
+The generator incorrectly imports Dict from models.dict instead of using typing.Dict
+in maps_api.py and storage_api.py. This shim provides the Dict type to make the
+generated code work without modification.
+
+This file can be removed when the generator bug is fixed in a future version.
+
+See: https://github.com/OpenAPITools/openapi-generator/issues (Dict import bug)
+"""
+
+from typing import Dict
+
+__all__ = ['Dict']
